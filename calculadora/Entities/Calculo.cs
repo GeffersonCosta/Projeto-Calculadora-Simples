8a31bc21b037a220429c007ca87daaa665dafb86
Gefferson Costa <geffersonhenrique9@gmail.com>
2021-08-11 18:25:37 +0100

    Projeto Calculadora

---
+++ calculadora/Entities/Calculo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace calculadora.Entities
+{
+    class Calculo
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+
+        public Calculo(int a, int b)
+        {
+            A = a;
+            B = b;
+        }
+        public double Operacao(double operador)
+        {
+            double soma = 0.0;
+         
+            switch (operador)
+            {
+                case 1:
+                    soma = A + B;
+                  
+                    break;
+                case 2:
+                    soma = A - B;
+                 
+                    break;
+                case 3:
+                    soma = A * B;
+                 
+                    break;
+                case 4:
+                    soma = A / B;
+                 
+                    break;
+            }
+            return soma;
+
+        }
+       
+    }
+}
---
+++ calculadora/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using calculadora.Entities;
+
+namespace calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Entre com o operador");
+            Console.WriteLine("1(+) 2(-) 3(*) 4(/) ");
+            Console.WriteLine("--------------------");
+            Console.Write(":");
+            int operador = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite os valores:");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Calculo cal = new Calculo(a, b);
+           
+            double soma = cal.Operacao(operador);
+            Console.WriteLine();
+
+            Console.WriteLine("Resultado = "+soma);
+
+        }
+    }
+}
